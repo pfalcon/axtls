b5ba024a9f29af317cef01f7e55813e396a99bae
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-06-10 21:09:45 +0300

    tls1: Abstract away getting last errno for a socket operation.
    For usage with embedded ports where socket library may have other way to
    pass errors rather than via C errno variable.

--- ssl/tls1.c
+++ ssl/tls1.c
@@ -918,7 +918,7 @@ static int send_raw_packet(SSL *ssl, uint8_t protocol)
 #ifdef WIN32
             if (GetLastError() != WSAEWOULDBLOCK)
 #else
-            if (errno != EAGAIN && errno != EWOULDBLOCK)
+            if (SOCKET_ERRNO() != EAGAIN && SOCKET_ERRNO() != EWOULDBLOCK)
 #endif
                 return SSL_ERROR_CONN_LOST;
         }
@@ -1150,7 +1150,7 @@ int basic_read(SSL *ssl, uint8_t **in_data)
 #ifdef WIN32
         if (GetLastError() == WSAEWOULDBLOCK)
 #else
-        if (errno == EAGAIN || errno == EWOULDBLOCK)
+        if (SOCKET_ERRNO() == EAGAIN || SOCKET_ERRNO() == EWOULDBLOCK)
 #endif
             return 0;
     }
