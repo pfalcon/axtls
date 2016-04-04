dd7b7125a477ae9c4bf73a2aafc346171ffada40
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-04-04 20:49:01 +0300

    crypto_misc.h: Don't ifdef in headers.

--- ssl/crypto_misc.h
+++ ssl/crypto_misc.h
@@ -120,14 +120,14 @@ typedef struct
 
 int x509_new(const uint8_t *cert, int *len, X509_CTX **ctx);
 void x509_free(X509_CTX *x509_ctx);
-#ifdef CONFIG_SSL_CERT_VERIFICATION
+//#ifdef CONFIG_SSL_CERT_VERIFICATION
 int x509_verify(const CA_CERT_CTX *ca_cert_ctx, const X509_CTX *cert, 
         int *pathLenConstraint);
-#endif
-#ifdef CONFIG_SSL_FULL_MODE
+//#endif
+//#ifdef CONFIG_SSL_FULL_MODE
 void x509_print(const X509_CTX *cert, CA_CERT_CTX *ca_cert_ctx);
 const char * x509_display_error(int error);
-#endif
+//#endif
 
 /**************************************************************************
  * ASN1 declarations 
