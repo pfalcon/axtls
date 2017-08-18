c421c13c63e5d659273ccff38e8000365cc87206
David Lechner <david@lechnology.com>
2017-08-17 22:26:36 -0500

    ssl: Fix unused variable warnings

--- ssl/tls1.c
+++ ssl/tls1.c
@@ -2012,12 +2012,14 @@ int process_certificate(SSL *ssl, X509_CTX **x509_ctx)
     int cert_size, offset = 5, offset_start;
     int total_cert_len = (buf[offset]<<8) + buf[offset+1];
     int is_client = IS_SET_SSL_FLAG(SSL_IS_CLIENT);
-    X509_CTX *chain = 0;
     X509_CTX **certs = 0;
     int *cert_used = 0;
     int num_certs = 0;
-    int i = 0;
     offset += 2;
+#ifndef CONFIG_SSL_SKELETON_MODE
+    X509_CTX *chain = 0;
+    int i = 0;
+#endif
 
     PARANOIA_CHECK(pkt_size, total_cert_len + offset);
 
@@ -2068,10 +2070,11 @@ int process_certificate(SSL *ssl, X509_CTX **x509_ctx)
 
     // third pass - link certs together, assume server cert is the first
     *x509_ctx = certs[0];
-    chain = certs[0];
     cert_used[0] = 1;
 
 #ifndef CONFIG_SSL_SKELETON_MODE
+    chain = certs[0];
+
     // repeat until the end of the chain is found
     while (1)
     {
